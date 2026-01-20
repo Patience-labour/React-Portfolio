bceda79da397d52573ed3fb72cb50e8c3055b4db
Patience-labour <maxonklaxon522@gmail.com>
2026-01-20 10:53:30 +0300

    fix all bags

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/React-Portfolio/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
